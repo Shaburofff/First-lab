4454bf3ceefe4ce43e4a66a1899af0d270df8238
Shaburofff <shaburov_98@list.ru>
2025-05-08 09:58:42 +0300

    first lab.py

--- first lab.py
+++ first lab.py
@@ -1,35 +1,25 @@
-import re
-
 DIGITS = {
     '0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре',
     '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'
 }
-
-PATTERN = r'\b97\d{2}\b'
 
 def number_to_words(number):
     return ' '.join(DIGITS[digit] for digit in number)
 
 def process_stream(filename):
     with open(filename, 'r') as file:
-        content = file.read()
-        
-    matches = re.findall(PATTERN, content)
+        data = file.read().split()
     
-    odd_numbers = []
-    for match in matches:
-        num = int(match)
-        if num % 2 != 0:  
-            odd_numbers.append(num)
-    
-    count = len(odd_numbers)
-    min_odd_number = str(min(odd_numbers)) if odd_numbers else None
-    
-    print("Подходящие числа:")
-    for n in sorted(odd_numbers):
-        print(n)
-    
-    print(f"\nКоличество чисел: {count}")
+    count = 0
+    min_odd_number = None
+
+    for item in data:
+        if item.isdigit() and len(item) == 4 and item.startswith("97") and int(item) % 2 != 0:
+            count += 1
+            if min_odd_number is None or int(item) < int(min_odd_number):
+                min_odd_number = item
+
+    print(f"Количество чисел: {count}")
     if min_odd_number:
         print("Минимальное число прописью:", number_to_words(min_odd_number))
 
